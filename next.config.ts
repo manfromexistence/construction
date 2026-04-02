15ac32c5a41481ef722b19bc8171ecadcbf99953
najmus-sakib-hossain <manfromexistence@proton.me>
2026-04-02 18:00:26 +0600

    Add image hostnames to Next.js config for ImgBB, Catbox, and Telegram

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,25 @@
 import { type NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "files.catbox.moe",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.telegram.org",
+        pathname: "/file/**",
+      },
+    ],
+  },
   turbopack: {
     rules: {
       '*.svg': {
